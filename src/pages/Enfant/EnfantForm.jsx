12a988bbc713ca41b1f9822c83fe19ea16251e28
izeriayounes <izeria.younes@ine.inpt.ma>
2023-08-28 12:12:04 +0100

    add more functionalities

---
+++ src/pages/Enfant/EnfantForm.jsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react';
+import Input from '../../components/Input';
+import AreaText from '../../components/AreaText';
+import Radio from '../../components/Radio';
+import ImgUpload from '../../components/imgUpload';
+import Button from '../../components/Button';
+import Select from '../../components/Select';
+import { useForm } from 'react-hook-form';
+import { post, put } from '../../api/apiService';
+import Loading from '../../components/Loading';
+
+function EnfantForm({ initialData }) {
+  const schoolLevels = [
+    'Primaire - 1ere',
+    'Primaire - 2eme',
+    'Primaire - 3eme',
+    'Primaire - 4eme',
+    'Primaire - 5eme',
+    'Primaire - 6eme',
+    'Collège - 1ere',
+    'Collège - 2eme',
+    'Collège - 3eme',
+    'Lycée - 5eme',
+    'Lycée - 1ere Bac',
+    'Lycée - 2eme Bac',
+  ];
+  const { register, handleSubmit, setValue } = useForm();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (initialData) {
+      Object.entries(initialData).forEach(([field, value]) => {
+        if (['dateDebutKafala', 'dateInscription', 'dateNaissance'].includes(field)) {
+          setValue(field, value?.split('T')[0]);
+          return;
+        }
+        setValue(field, value);
+      });
+    }
+  }, [initialData, setValue]);
+
+  const handleFormSubmit = async (data) => {
+    try {
+      setIsLoading(true);
+      if (initialData) {
+        await put(`enfants/${initialData.id}`, data);
+      } else {
+        await post('enfants', data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex-1 justify-center">
+      {isLoading && <Loading />}
+      <h1 className="mb-4">Veuillez remplir les informations d'enfant:</h1>
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <div className="md:flex md:space-x-6 mb-4">
+          <Input className="flex-grow md:mb-0" label="Nom" id="nom" type="text" register={register} required />
+          <Input className="flex-grow md:mb-0" label="Prenom" id="prenom" type="text" register={register} required />
+        </div>
+
+        <Radio register={register} required />
+
+        <Input label="Date de naissance" id="dateNaissance" type="date" register={register} />
+        <Input label="Lieu de naissance" id="lieuNaissance" type="text" register={register} />
+        <Select label="Niveau scolaire" id="niveauScolaire" register={register} list={schoolLevels} />
+        <Input label="Date d'inscription" id="dateInscription" type="date" register={register} />
+        <Input label="Date debut parrainage" id="dateDebutKafala" type="date" register={register} />
+
+        <AreaText label="Remarques" id="remarque" register={register} />
+        <ImgUpload onChange={(encodedData) => setValue('profilePicture', encodedData)} />
+        <div className="flex justify-end my-5">
+          <Button primary>{!initialData ? 'Enregistrer' : 'Enregister les modifications'}</Button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+function EnfantCreate() {
+  return <EnfantForm />;
+}
+function EnfantEdit({ initialData }) {
+  return (
+    <div className="pt-12 pl-12">
+      <EnfantForm initialData={initialData} />;
+    </div>
+  );
+}
+
+export { EnfantCreate, EnfantEdit };
+
+export default EnfantForm;
